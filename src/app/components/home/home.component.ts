fab4924eff0b1509094cacc454cc68dfea2b372e
Web Master <webmaster@dc1rec.local>
2019-12-28 10:54:48 -0600

    home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,6 @@ import { PdfMakeWrapper, QR } from 'pdfmake-wrapper';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -37,49 +36,80 @@ export class HomeComponent implements OnInit {
   getOne( form: NgForm ) {
 
     this.mysql.getOneS(this.matricula).subscribe( data => {
-
-      this.data.push(data);
-      console.log(data);
       
+      Swal.fire({
+        title: 'Correcto',
+        html: '<p>Matricula validada, cargando datos, por favor espere...</p><br><div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>',
+        icon: 'success',
+        timer: 2000,
+        allowOutsideClick: false,
+        showConfirmButton: false,
+        onClose: () => {
+          this.tiket = true;
+          this.data.push(data);
+          console.log(data);
+        }
+      });
     }, error => {
 
-      this.tiket = false;
-      console.log(this.tiket);
-
-      //console.log('tu matricula no coincide');
       Swal.fire({
         title: 'Error!',
-        text: 'Tu matricula no esta registrada en nuestra base de datos',
+        text: 'Tu matricula no coincide con alguna registrada en nuestra base de datos',
         icon: 'error',
-        confirmButtonText: 'volver a intentar'
+        confirmButtonText: 'volver a intentar',
+        allowOutsideClick: false,
+        onClose: ()=> {
+          this.tiket = false;
+          console.log(this.tiket);
+        }
       });
-
     });
 
   }
 
   imprimir(){
-    this.mysql.updateImp(this.matricula, this.impresion)
-  }
 
-  getPdf() {
+    let m = this.data[0][0].matricula;
+    this.mysql.consultaImp(m).subscribe( data => {
+      
+      var imp = data[0].impresion;
 
-    let n= this.data[0][0].nombre;
-    let f= this.data[0][0].folio;
+      if (imp >= '4' ) {
+        //console.log(imp);
+        Swal.fire({
+          title: 'Upss',
+          text: 'Parece que has llegado al limite de impresiones, recargando la pagina.',
+          icon: 'warning',
+          allowOutsideClick: false,
+          onClose: ()=> {
+            location.reload();
+          }
+        });
+      }else {
+        this.mysql.updateImp(m, imp).subscribe( ()=>{
+          console.log('actualizado');
 
-    console.log(n);
+          PdfMakeWrapper.setFonts(pdfFonts);
+          const pdf = new PdfMakeWrapper();
+      
+          pdf.pageMargins([ 40, 60, 40, 60 ]);
+          pdf.header('Boletos para Graduación UNIMEX');
+          pdf.footer('UNIMEX, todos los derechos reservados.');
+          var add = {
+            content: [
+              
+            ]
+          }
+          pdf.rawContent(m);
+          let titulo = 'invitaciones-UNIMEX'+m+'.pdf';
+          pdf.create().download(titulo);
 
-    // Set the fonts to use
-    PdfMakeWrapper.setFonts(pdfFonts);
+        }, error => {
+          console.log(error);
+        });
+      }
 
-    const pdf = new PdfMakeWrapper();
-
-    pdf.pageMargins([ 40, 60, 40, 60 ]);
-    pdf.header('Boletos para Graduación UNIMEX');
-    pdf.add('Tus datos son:');
-    pdf.rawContent(n);
-    pdf.create().download();
-
+    });
   }
 
 }
